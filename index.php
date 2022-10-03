2f0a617faae36bed38fb9323e04f9d770311c691
Ivan Filatov <introvert696@yandex.ru>
2022-10-03 14:04:16 +0400

    dasd

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
-//i use register branch
+
 require_once 'Controllers/router.php';
+
 
 //запускаем роутер, и там все обрабатываем
 $router = new Route();
 $router->start();
+//test
+//i use master branch
